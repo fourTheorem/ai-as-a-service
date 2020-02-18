65dc30636e738ee7c462c9843726369864babba2
Peter Elger <elger.peter@gmail.com>
2020-02-18 12:29:43 +0000

    addtional corrections to chapter 7 environment

--- chapter7/pipeline/training/status.sh
+++ chapter7/pipeline/training/status.sh
@@ -3,6 +3,6 @@
 . ../.env
 . checkenv.sh
 
-export CHAPTER6_CLASSIFIER_ARN
+export CHAPTER7_CLASSIFIER_ARN
 node ./check-status.js
 
--- chapter7/pipeline/training/train.sh
+++ chapter7/pipeline/training/train.sh
@@ -3,10 +3,10 @@
 . ../.env
 . checkenv.sh
 
-export CHAPTER6_PIPELINE_TRAINING_BUCKET
-export CHAPTER6_PIPELINE_CLASSIFIER_BUCKET
-export CHAPTER6_CLASSIFIER_NAME
-export CHAPTER6_DATA_ACCESS_ARN
+export CHAPTER7_PIPELINE_TRAINING_BUCKET
+export CHAPTER7_PIPELINE_CLASSIFIER_BUCKET
+export CHAPTER7_CLASSIFIER_NAME
+export CHAPTER7_DATA_ACCESS_ARN
 
 # TODO: write the classifier arn to the ./env file for downstream use
 node ./train-classifier.js
